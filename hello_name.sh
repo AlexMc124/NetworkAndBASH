1d1e8e267837283f3071b20efd334d7eb828e9ac
Alex McIntosh <alex.mcintosh@krakentechnologies.ltd>
2023-01-28 19:33:19 +0000

    Inital commit with some other goofy scripts for text

---
+++ hello_name.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+echo "What is your name?"
+read name 
+echo "How do you $name"
+read remark
+echo "I am $remark too"
---
+++ name_and_age.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+name="Alex"
+age=25
+
+echo "My name is $name and my age is $age"
